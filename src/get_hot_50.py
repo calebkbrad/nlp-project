36f08fd4a2a92b51122fae573819a22da0286a02
caleb <cbrad022@odu.edu>
2024-11-04 21:58:42 -0500

    support multiple subreddits

--- src/get_hot_50.py
+++ src/get_hot_50.py
@@ -8,29 +8,29 @@ import praw.models
 
 def main(args):
     reddit = praw.Reddit("scraper", user_agent="ubuntu:nlp-project:v1.0.0.0 (by u/calebkbradford1)")
-    subreddit_string = args[0]
-    to_dump = {}
-    for submission in reddit.subreddit(subreddit_string).hot(limit=50):
-        comments = []
-        if submission.stickied:
-            continue
-        current_submission = {}
-        current_submission['title'] = submission.title
-        current_submission['text'] = submission.selftext
-        for comment in submission.comments.list():
-            if type(comment) != praw.models.Comment:
+    for subreddit_string in args:
+        to_dump = {}
+        for submission in reddit.subreddit(subreddit_string).hot(limit=50):
+            comments = []
+            if submission.stickied:
                 continue
-            comments.append(comment.body)
-        current_submission['comments'] = comments
-        to_dump[submission.id] = current_submission
-    
-    now = datetime.now()
-    date_string = now.strftime("%m_%d_%H:%M")
-    if not os.path.exists(f"data/{subreddit_string}"):
-        os.makedirs(f"data/{subreddit_string}")
-    with open(f"data/{subreddit_string}/{date_string}.json", 'w') as writer:
-        json.dump(to_dump, writer, indent=3)
-    
+            current_submission = {}
+            current_submission['title'] = submission.title
+            current_submission['text'] = submission.selftext
+            for comment in submission.comments.list():
+                if type(comment) != praw.models.Comment:
+                    continue
+                comments.append(comment.body)
+            current_submission['comments'] = comments
+            to_dump[submission.id] = current_submission
+        
+        now = datetime.now()
+        date_string = now.strftime("%m_%d_%H:%M")
+        if not os.path.exists(f"data/{subreddit_string}"):
+            os.makedirs(f"data/{subreddit_string}")
+        with open(f"data/{subreddit_string}/{date_string}.json", 'w') as writer:
+            json.dump(to_dump, writer, indent=3)
+        
     
 
 
